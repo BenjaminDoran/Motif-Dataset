8c398ea473a45e5388bfb812e2195f8ba02165ef
Benjamin Doran <benadoran@gmail.com>
2017-08-02 23:17:07 -0400

    added regulonDB class template

---
+++ regulondb_import.py
@@ -0,0 +1,27 @@
+"""
+Author: Benjamin Doran
+Name: regulondb_import.py
+Date: Aug 2017
+Purpose: Pull data from RegulonDB database
+Output: 2 csv files
+Data Source: RegulonDB <http://regulondb.ccg.unam.mx/>
+"""
+import pandas as pd
+import re
+
+class RegulonDBImporter:
+    def __init__(self):
+        """ TODO """
+        pass
+
+    def import_data():
+        """ TODO """
+        pass
+
+    def load_regulon_data():
+        """ TODO """
+        pass
+
+    def export()
+        """ TODO """
+        pass
