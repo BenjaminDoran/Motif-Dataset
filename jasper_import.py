5ef1e0f1a7d2bdd228f55fa95e65579e3cb79799
Benjamin Doran <benadoran@gmail.com>
2017-08-27 18:03:11 -0400

    Squashed commit of the following: (Improved Jasper_importer.py, which now works in pulling in all bed file info commit 413c78391f4c4f2f00bc8e9d055a98408ce9d651 Author: Benjamin Doran <benadoran@gmail.com> Date:   Fri Aug 4 12:44:48 2017 -0400
        reorder functions
    
    commit d7b2025f94d3ca06edf8bc2d1caf273b91d93799
    Author: Benjamin Doran <benadoran@gmail.com>
    Date:   Fri Aug 4 12:32:52 2017 -0400
    
        added switch_org_name
    
    commit 9e7421f31db3b72a4ca6595dbc02357811c1345d
    Author: Benjamin Doran <benadoran@gmail.com>
    Date:   Thu Aug 3 19:48:35 2017 -0400
    
        repulled jasper data
    
    commit 7b83b07ed612d512000367be4c43b7f8b446c9a7
    Author: Benjamin Doran <benadoran@gmail.com>
    Date:   Thu Aug 3 17:22:05 2017 -0400
    
        refactoring jasper_import

--- jasper_import.py
+++ jasper_import.py
@@ -14,70 +14,94 @@ from bs4 import BeautifulSoup
 
 class JasperImporter:
     # startup
-    def __init__(self, url = "http://jaspar.genereg.net/html/DOWNLOAD/", bedfilename = "motif-data.csv"):
+    def __init__(self, url="http://jaspar.genereg.net/html/DOWNLOAD/",
+                 bedfilename="motif-data.csv", createnew=True):
         """ Setup Importer """
         self.url = url
         self.bedfilename = bedfilename
 
-        if os.path.isfile(bedfilename):
+        if createnew:
+            self.df = pd.DataFrame([], columns=["motif-id", "organism",
+                                                "chromosome", "start",
+                                                "stop", "strand"])
+        elif os.path.isfile(bedfilename) and not createnew:
             self.load_csv()
         else:
-            self.df = None
+            print('No data to load, acting in createnew mode')
+            self.df = pd.DataFrame([])
 
     # functions
     def load_csv(self) -> bool:
         """ reads in data.csv """
-        try:
-            self.df = pd.read_csv(self.bedfilename, sep = ",")
-            return True
-        except:
-            return False
+        self.df = pd.read_csv(self.bedfilename, sep=",")
+        return True
 
     def load_jasper_bed_files(self) -> bool:
-        """loads jasper bed files which contain jasper motif id
-        and locus of motif, writes information to .csv file
+        """ 
+        loads jasper bed files which contain
+        jasper motif-id and query of motif
         """
-        if self.df != None:
-            print("bed files already loaded from csv file")
-            return False
-        else:
-            bed_url = self.url + "bed_files/"
-            self.df = pd.DataFrame([])
-            final_cols = ["motif-id", "organism",
-                          "chromosome", "start",
-                           "stop", "strand"]
-            def strand_search(y):
-                try:
-                    return re.search("\+|\-", x["query"]).group(0)
-                except AttributeError:
-                    return None
+        bed_url = self.url + "bed_files/"
+        final_cols = ["motif-id","organism","chromosome","start","stop","strand"]
 
-            # enter bed directory
-            response = requests.get(bed_url)
-            soup = BeautifulSoup(response.content, "html.parser")
-            # all links to files
-            bed_motifs = soup.find_all("a", text = re.compile("MA."))
-            # for link to file
-            for a in bed_motifs:
-                locus_file = pd.read_csv(bed_url + a.text, sep = "\t", header = None)
-                try: # files have either 4 or 6 columns
-                    locus_file.columns = ["chromosome", "start", "stop", "query", "frame", "strand"]
-                except ValueError:
-                    locus_file.columns = ["chromosome", "start", "stop", "query"]
-                # parse query for organism name (and potentially strand)
-                locus_file["organism"] = locus_file.loc[:,"query"].str.split(r"_", expand = True).loc[:,0]
+        # enter bed directory
+        response = requests.get(bed_url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        # all links to files
+        bed_motifs = soup.find_all("a", text=re.compile("MA."))
+        # for link to file
+        for a in bed_motifs:
+            # ignore files that do not follow format (-400 entries)
+            if a.text not in ("MA0548.1.bed","MA0555.1.bed","MA0563.1.bed"):
+                # read csv
+                locus_file = pd.read_csv(bed_url+a.text, sep="\t", header=None)
+
+                # query is column 3 (0 indexed)
+                locus_file.rename(columns={3: "query"}, inplace=True)
+
+                # query -> organism, chormosome, start, stop, strand
+                result_df = self.expand_query(locus_file["query"])
+
+                # name columns
+                result_df.columns = final_cols[1:]
+                
                 # motif id is part of file name
-                locus_file["motif"] = a.text[:-4]
-                # only + or - in query indicates strand
-                locus_file["strand"] = locus_file.apply(lambda x: strand_search(x), axis = 1)
-                self.df = self.df.append(pd.DataFrame(locus_file.loc[:,final_cols]), ignore_index=True)
-            self.df.columns = final_cols
+                result_df["motif-id"] = a.text[:-4]
+
+                # reorder columns
+                result_df = result_df[final_cols]
+
+                # append new columns
+                self.df = self.df.append(result_df, ignore_index=True)
         return True
 
     def load_jasper_pfm_files(self) -> bool:
         """ TODO """
         return False
 
+    def expand_query(self, query:pd.Series) -> pd.DataFrame:
+        """ Parses query formatted for UCSC Biologic database """
+        # expand query
+        parse_query = re.compile("[a-zA-Z0-9]+|\([\+\-]\)")
+        results = query.apply(parse_query.findall).apply(pd.Series)
+
+        # get strand without "()"
+        restrip = re.compile("[\+\-]")
+        results[4] = results[4].apply(lambda y: restrip.search(y).group(0))
+        
+        return results
+
+    def switch_org_name(self, orgs:pd.Series) -> pd.Series:
+        """ Converts UCSC style org ref names to Latin Names """
+        name_table = {'tair10': 'arabidopsis thaliana', 
+                      'at': 'arabidopsis thaliana', 
+                      'hg19': 'homo sapian', 
+                      'ce': 'caenorhabditis elegans ', 
+                      'mm9': 'mus muscosis', 
+                      'dm': 'drosophila melanogaster'}
+
+        return orgs.apply(lambda x: name_table[x])
+
     def export(self) -> bool:
         """ TODO """
-        self.df.to_csv(self.bedfilename, sep = ",", header = True, index = False)
+        self.df.to_csv(self.bedfilename, sep=",", header=True, index=False)
