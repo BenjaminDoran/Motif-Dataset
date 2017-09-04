de9619bf5c2e8d6695dd38336f02d32d76068232
Benjamin Doran <benadoran@gmail.com>
2017-09-04 00:32:56 -0400

    significantly updated style

--- jasper_import.py
+++ jasper_import.py
@@ -6,45 +6,71 @@ Purpose: pull bed files and pfm files from Jasper database
 Output: 2 .csv files (pfm.csv, motif-data.csv)
 Data Source: Jasper database, http://jaspar.genereg.net
 """
-import pandas as pd
+
 import os.path
 import re
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
+
+def switch_org_name(orgs: pd.Series) -> pd.Series:
+    """ Converts UCSC style org ref names to Latin Names """
+    name_table = {'tair10': 'arabidopsis thaliana',
+                  'at': 'arabidopsis thaliana',
+                  'hg19': 'homo sapiens',
+                  'ce': 'caenorhabditis elegans',
+                  'mm9': 'mus musculus',
+                  'dm': 'drosophila melanogaster'}
+
+    return orgs.apply(lambda x: name_table[x])
+
+
+def expand_query(query: pd.Series) -> pd.DataFrame:
+    """ Parses query formatted for UCSC Biologic database """
+    # expand query
+    parse_query = re.compile(r"[a-zA-Z0-9]+|\([\+\-]\)")
+    results = query.apply(parse_query.findall).apply(pd.Series)
+
+    # get strand without "()"
+    restrip = re.compile(r"[\+\-]")
+    results[4] = results[4].apply(lambda y: restrip.search(y).group(0))
+    return results
+
+
 class JasperImporter:
+    """ TODO """
     # startup
-    def __init__(self, 
-                 url="http://jaspar.genereg.net/html/DOWNLOAD/",
-                 bedfilename="motif-data.csv", 
-                 createnew=True):
+    def __init__(self, url="http://jaspar.genereg.net/html/DOWNLOAD/",
+                 bedfilename="motif-data.csv", createnew=True):
         """ Setup Importer """
         self.url = url
         self.bedfilename = bedfilename
 
         if createnew:
-            self.df = pd.DataFrame([], columns=["motif-id", "organism",
-                                                "chromosome", "start",
-                                                "stop", "strand"])
+            self.datf = pd.DataFrame([], columns=["motif-id", "organism",
+                                                  "chromosome", "start",
+                                                  "stop", "strand"])
         elif os.path.isfile(bedfilename) and not createnew:
             self.load_csv()
         else:
             print('No data to load, acting in createnew mode')
-            self.df = pd.DataFrame([])
+            self.datf = pd.DataFrame([])
 
     # functions
     def load_csv(self) -> bool:
         """ reads in data.csv """
-        self.df = pd.read_csv(self.bedfilename, sep=",")
+        self.datf = pd.read_csv(self.bedfilename, sep=",")
         return True
 
     def load_jasper_bed_files(self) -> bool:
-        """ 
+        """
         loads jasper bed files which contain
         jasper motif-id and query of motif
         """
         bed_url = self.url + "bed_files/"
-        final_cols = ["motif-id","organism","chromosome","start","stop","strand"]
+        final_cols = ["motif-id", "organism", "chromosome",
+                      "start", "stop", "strand"]
 
         # enter bed directory
         response = requests.get(bed_url)
@@ -52,60 +78,42 @@ class JasperImporter:
         # all links to files
         bed_motifs = soup.find_all("a", text=re.compile("MA."))
         # for link to file
-        for a in bed_motifs:
+        for link in bed_motifs:
             # ignore files that do not follow format (-400 entries)
-            if a.text not in ("MA0548.1.bed","MA0555.1.bed","MA0563.1.bed"):
+            if link.text not in (
+                "MA0548.1.bed", "MA0555.1.bed", "MA0563.1.bed"
+            ):
                 # read csv
-                locus_file = pd.read_csv(bed_url+a.text, sep="\t", header=None)
+                locus_file = pd.read_csv(bed_url+link.text,
+                                         sep="\t",
+                                         header=None)
 
                 # query is column 3 (0 indexed)
                 locus_file.rename(columns={3: "query"}, inplace=True)
 
                 # query -> organism, chormosome, start, stop, strand
-                result_df = self.expand_query(locus_file["query"])
+                result_df = expand_query(locus_file["query"])
 
                 # name columns
                 result_df.columns = final_cols[1:]
-                
+
                 # motif id is part of file name
-                result_df["motif-id"] = a.text[:-4]
+                result_df["motif-id"] = link.text[:-4]
 
                 # reorder columns
                 result_df = result_df[final_cols]
 
+                # switch org names to Latin
+                result_df["organism"] = switch_org_name(result_df["organism"])
+
                 # append new columns
-                self.df = self.df.append(result_df, ignore_index=True)
+                self.datf = self.datf.append(result_df, ignore_index=True)
         return True
 
     def load_jasper_pfm_files(self) -> bool:
         """ TODO """
         return False
 
-    def expand_query(self, query:pd.Series) -> pd.DataFrame:
-        """ Parses query formatted for UCSC Biologic database """
-        # expand query
-        parse_query = re.compile("[a-zA-Z0-9]+|\([\+\-]\)")
-        results = query.apply(parse_query.findall).apply(pd.Series)
-
-        # get strand without "()"
-        restrip = re.compile("[\+\-]")
-        results[4] = results[4].apply(lambda y: restrip.search(y).group(0))
-        
-        return results
-
-    def switch_org_name(self, orgs:pd.Series) -> pd.Series:
-        """ Converts UCSC style org ref names to Latin Names """
-        name_table = {'tair10': 'arabidopsis thaliana', 
-                      'at': 'arabidopsis thaliana', 
-                      'hg19': 'homo sapiens', 
-                      'ce': 'caenorhabditis elegans', 
-                      'mm9': 'mus muscosis', 
-                      'dm': 'drosophila melanogaster'}
-
-        return orgs.apply(lambda x: name_table[x])
-
     def export(self) -> bool:
         """ TODO """
-        self.df.to_csv(self.bedfilename, sep=",", header=True, index=False)
-
-
+        self.datf.to_csv(self.bedfilename, sep=",", header=True, index=False)
--- ncbi_import.py
+++ ncbi_import.py
@@ -2,77 +2,98 @@
 Author: Benjamin Doran
 Name: ncbi_import.py
 Date: July 2017
-Purpose: take organism + chromosome + posistion info -> return dataset of sequences
-Output: .csv file (sequences.csv)
+Purpose: take organism + chromosome + posistion info ->
+         return dataset of sequences
+Output: .csv file (sequences.csv) columns: label, sequence
 Data Source: NCBI Entrez -- https://www.ncbi.nlm.nih.gov/
 """
 
 from Bio import Entrez, SeqIO
+from Bio.Seq import Seq
 import pandas as pd
 
-""" handle = Entrez.esearch(db='nucleotide', term='{}[Orgn] AND chromosome {}') -> id 
-    record = Entrez.read(handle)
-    record["IdList"]
-"""
-""" Entrez.efetch(db='nucleotide',
-                  id='returned id',
-                  rettype="gb",
-                  retmode='text')
-"""
+class NCBIimporter:
+    """ TODO """
 
-class NCBI_Importer:
-    # conversion table for organism + chromosome to Accesion number
-    # at this point it does not seem to feasible to do this with biopython
-    self.org_to_id = {
-        'arabidopsis thaliana': lambda n: \
-                                    "CP00268{:01d}".format(int(n)+3) if \
-                                        (n.isnumeric()) & (0 < int(n) < 6) else \
-                                    None,
-        'caenorhabditis elegans': lambda n: \
-                                    "NC_003279" if n == "I"   else \
-                                    "NC_003280" if n == "II"  else \
-                                    "NC_003281" if n == "III" else \
-                                    "NC_003282" if n == "IV"  else \
-                                    "NC_003283" if n == "V"   else \
-                                    "NC_003284" if n == "X"   else \
-                                    None,
-        'drosophila melanogaster': lambda n: \
-                                    "NC_004354" if n == "X"  else \
-                                    "NT_033778" if n == "2R" else \
-                                    "NT_033777" if n == "3R" else \
-                                    "NT_033779" if n == "2L" else \
-                                    "NT_037436" if n == "3L" else \
-                                    "NC_004353" if n == "4"  else \
-                                    None,
-        'escherichia coli': lambda n = None: "NC_000913",
-        'homo sapiens': lambda n: \
-                        "NC_0000{:02d}".format(int(n)) if \
-                            (n.isnumeric()) & (0 < int(n) < 23) else \
-                        "NC_000023" if n == "X" else \
-                        "NC_000024" if n == "Y" else \
-                        None,
-        'mus musculus': lambda n: \
-                        "NC_0000{:02d}".format(int(n)+66) if \
-                            (n.isnumeric()) & (0 < int(n) < 20) else \
-                        "NC_000086" if n == "X" else \
-                        "NC_000087" if n == "Y" else \
-                        None
+    id_switch = {
+        # conversion table for organism + chromosome to RefSeq or GI id
+        # it does not seem to feasible to do this with
+        # biopython searching programmatically
+        # thale cress (plant)
+        'arabidopsis thaliana':
+            lambda chrom:
+            "CP00268{:01d}".format(int(chrom)+3) if
+            chrom.isnumeric() and (0 < int(chrom) < 6) else
+            None,
+        # c. elegans (worm)
+        'caenorhabditis elegans':
+            lambda chrom:
+            "NC_003279" if chrom == "I" else
+            "NC_003280" if chrom == "II" else
+            "NC_003281" if chrom == "III" else
+            "NC_003282" if chrom == "IV" else
+            "NC_003283" if chrom == "V" else
+            "NC_003284" if chrom == "X" else
+            None,
+        # fruit fly
+        'drosophila melanogaster':
+            lambda chrom:
+            "NC_004354" if chrom == "X" else
+            "NT_033778" if chrom == "2R" else
+            "NT_033777" if chrom == "3R" else
+            "NT_033779" if chrom == "2L" else
+            "NT_037436" if chrom == "3L" else
+            "NC_004353" if chrom == "4" else
+            None,
+        # e. coli (bacteria) - no chromosomes
+        'escherichia coli': lambda chrom=None: "NC_000913",
+        # human
+        'homo sapiens':
+            lambda chrom:
+            "NC_0000{:02d}".format(int(chrom)) if
+            chrom.isnumeric() and (0 < int(chrom) < 23) else
+            "NC_000023" if chrom == "X" else
+            "NC_000024" if chrom == "Y" else
+            None,
+        # house mouse
+        'mus musculus':
+            lambda chrom:
+            "NC_0000{:02d}".format(int(chrom)+66) if
+            chrom.isnumeric() and (0 < int(chrom) < 20) else
+            "NC_000086" if chrom == "X" else
+            "NC_000087" if chrom == "Y" else
+            None
     }
 
-    def __init__(self, user_email:str, in_file:str):
+    def __init__(self, user_email: str, in_file: str):
+        """ Requires user_email and file of motif locations """
+        # Setup
         Entrez.email = user_email
-        self.df = pd.read_csv(in_file, sep=",")
+        self.locdat = pd.read_csv(in_file, sep=",")
+        self.seqdat = pd.DataFrame()
 
-    def search_chromosomes(self) -> bool:
+    def org_to_id(self, org: str, chrom: str) -> str:
+        """ get RefSeq or GI id from organism and chromosome """
+        return self.id_switch[org](chrom)
+
+    def _search_chromosome(self) -> bool:
         """ TODO """
         return False
 
-    def load_chromosomes(self) -> bool:
-        """ TODO """
-        return False
+    def _load_chromosome(self, iden: str):
+        """ fetch fasta sequence from NCBI given id """
+        with Entrez.efetch(db='nucleotide', id=iden, rettype="fasta") as handle:
+            return Seq(SeqIO.read(handle, 'fasta'))
 
     def make_sequences(self) -> bool:
         """ TODO """
+        # sort locdat by chromosome
+        # for each chromosome:
+        #   org_to_id
+        #   _load_chromosome
+        #   _search_chromosome
+        #   _add_label_seq
+        # export_sequences
         return False
 
     def export_sequences(self) -> bool:
@@ -82,6 +103,3 @@ class NCBI_Importer:
 
 if __name__ == "__main__":
     print("TODO")
-
-
-
