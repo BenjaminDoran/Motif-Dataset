9940e60f949e00e8e7be56b32fca2fb3aa07cd65
Benjamin Doran <benadoran@gmail.com>
2017-09-08 01:15:31 -0400

    load_data.py updated to save ncbi sequences

--- load_data.py
+++ load_data.py
@@ -8,11 +8,11 @@ Data Source: "motif-data.csv", NCBI-Entrez <>
 """
 import sys
 from jasper_import import JasperImporter
+from ncbi_import import NCBIimporter
 
-def import_jasper_loc_data() -> bool:
+def import_jasper_loc_data(url, outfile) -> bool:
     """ fetch motif locations from jasper database """
-    jas_imp = JasperImporter("http://jaspar.genereg.net/html/DOWNLOAD/",
-                             "motif-data.csv")
+    jas_imp = JasperImporter(url, outfile)
 
     worked = jas_imp.load_jasper_bed_files()
     if not worked:
@@ -25,11 +25,45 @@ def import_jasper_loc_data() -> bool:
         return False
     return True
 
+def ncbi_save_sequences(email, infile):
+    """ fetch NCBI chromosome sequences and save in sequences folder """
+    ncbi_imp = NCBIimporter(email, infile)
+    return ncbi_imp.save_sequences()
+
+
 
 def main():
     """ TODO """
-    if not import_jasper_loc_data():
+    # get user email
+    email = ""
+    while not email:
+        email = input("Please enter your email address: ")
+
+    # confirm location file name
+    locfile = input("pulling from 'motif-data.csv' if this is not correct \
+                    please type the file name and press enter,\
+                    else just press enter:\n")
+    if not locfile:
+        locfile = "motif-data.csv"
+
+    # hard code jaspers url
+    jasper_url = "http://jaspar.genereg.net/html/DOWNLOAD/"
+
+    # fetch jasper motif location data
+    if not import_jasper_loc_data(jasper_url, locfile):
         sys.exit(1)
+
+    # From location data fetch and save chromosome sequences
+    if ncbi_save_sequences(email, locfile) < 1:
+        sys.exit(2)
+
+    # from saved sequences make final data file
+    # columns:
+    # motif-id: unique id of sequence
+    # sequence: actual sequence
+    # chromosome-index: in form of <start>:<stop><strand>
+    # mstart: index in sequence where motif starts
+    # mstop: index in sequence where motif stops
 
 if __name__ == "__main__":
     main()
